17bbd88675c39b08ca85a59f1ef51fcfa417ef9b
Vidyut <vidyutlearning07@gmail.com>
2023-10-14 16:35:50 +0530

    updated packages and fix

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import user from "../images/   ";
+import user from "../images/user.png";
 
 const ContactCard = (props) => {
   const { id, name, email } = props.contact;
